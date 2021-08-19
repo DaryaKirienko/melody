a69a17caad21a82715b03ad968c1a2c11536bdac
DaryaKirienko <83349263+DaryaKirienko@users.noreply.github.com>
2021-08-19 14:32:24 +0300

    Добавила изменение счетчика при клике на этаж

--- js/main.js
+++ js/main.js
@@ -3,11 +3,24 @@ $(document).ready(function () {
   var floorPath = $(".home-image path");
   var counterUp = $(".counter-up");
   var counterDown = $(".counter-down");
+  let isClicked = false;
 
   floorPath.on("mouseover", function () {
+    if (isClicked) {
+      return;
+    }
     floorPath.removeClass("current-floor");
+
     currentFloor = $(this).attr("data-floor");
     $(".counter").text(currentFloor);
+  });
+
+  floorPath.on("click", function () {
+    floorPath.removeClass("current-floor");
+    $(this).toggleClass("current-floor");
+    currentFloor = $(this).attr("data-floor");
+    $(".counter").text(currentFloor);
+    isClicked = true;
   });
 
   counterUp.on("click", function () {
